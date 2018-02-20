8fcf91c9ec712e0ab42d63332003a1b6ed0a886a
Alex Lyzhoft <alexlyz@gmail.com>
2018-02-19 20:18:05 -0600

    Refactered for recycler view adapter

--- app/src/main/java/com/example/gangelf/coderswag/Adapters/CategoryAdapter.kt
+++ app/src/main/java/com/example/gangelf/coderswag/Adapters/CategoryAdapter.kt
@@ -13,10 +13,7 @@ import com.example.gangelf.coderswag.R
 /**
  * Created by Gangelf on 2/18/2018.
  */
-class CategoryAdapter(context: Context, categories: List<Category>) : BaseAdapter(){
-
-    val context = context
-    val categories = categories
+class CategoryAdapter(val context: Context, val categories: List<Category>) : BaseAdapter(){
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         val categoryView: View
--- app/src/main/java/com/example/gangelf/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/example/gangelf/coderswag/Controller/MainActivity.kt
@@ -2,9 +2,11 @@ package com.example.gangelf.coderswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
 import android.widget.ArrayAdapter
 import android.widget.Toast
 import com.example.gangelf.coderswag.Adapters.CategoryAdapter
+import com.example.gangelf.coderswag.Adapters.CategoryRecycleAdapter
 import com.example.gangelf.coderswag.Model.Category
 import com.example.gangelf.coderswag.R
 import com.example.gangelf.coderswag.Services.DataService
@@ -13,24 +15,23 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     //Simple Adapter
-//    lateinit var adapter : ArrayAdapter<Category>
-    lateinit var adapter : CategoryAdapter
+    //lateinit var adapter : ArrayAdapter<Category>
+    lateinit var adapter : CategoryRecycleAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-//      Simple Adapter
+//
+//      //Simple Adapter
 //        adapter = ArrayAdapter(this,
 //                android.R.layout.simple_list_item_1,
 //                DataService.categories)
 
-        adapter = CategoryAdapter(this, DataService.categories)
+        adapter = CategoryRecycleAdapter(this, DataService.categories)
         categorieListView.adapter = adapter
 
-//            categorieListView.setOnItemClickListener { adapterView, view, i, l ->
-//            val category = DataService.categories[i]
-//            Toast.makeText(this, "You clicked on the ${category.title} cell", Toast.LENGTH_SHORT).show()
-//        }
+        val layoutManager = LinearLayoutManager(this)
+        categorieListView.layoutManager = layoutManager
+        categorieListView.setHasFixedSize(true)
     }
 }
